5430fa40cf7fa2ca6cd57540a958a9f484c9529c
EC2 Default User <ec2-user@ip-172-31-46-36.ap-northeast-1.compute.internal>
2024-09-25 08:52:04 +0000

    検索機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
    devise_for :users
     root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
+  get "search" => "searches#search"
 
  resources :books, only: [:new, :index, :show, :create, :edit, :update,:destroy] do
   resource :favorites, only: [:create, :destroy]
