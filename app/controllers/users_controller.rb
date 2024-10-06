9a377c5b98f91b162a7f0594a0486250b2eec0b3
EC2 Default User <ec2-user@ip-172-31-46-36.ap-northeast-1.compute.internal>
2024-10-06 09:01:21 +0000

    コメント投稿、いいね機能の非同期通信実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book1 = Book.new
     @books = @user.books
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
