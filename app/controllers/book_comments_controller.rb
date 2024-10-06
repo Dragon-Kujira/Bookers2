2abba33dc157197f96c854aaf6d644b60f8b55ae
EC2 Default User <ec2-user@ip-172-31-46-36.ap-northeast-1.compute.internal>
2024-10-06 08:01:09 +0000

    コメント投稿、いいね機能の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,23 @@
 class BookCommentsController < ApplicationController
-
+  
   def create
-    book = Book.find(params[:book_id])
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    @book_comment.save
+    #redirect_back(fallback_location: root_path)
   end
-
-  def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
-  end
-
+  
+def destroy
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    #redirect_back(fallback_location: root_path)
+end
+  
   private
-
+  
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
